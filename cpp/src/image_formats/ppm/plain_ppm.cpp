e8203448ec4a07638befce88384a99c9807438fd
Lawrence Rafferty <lawrence.rafferty@gmail.com>
2019-05-29 23:29:10 -0700

    Support writing plain PPM image format pixel data.

--- cpp/src/image_formats/ppm/plain_ppm.cpp
+++ cpp/src/image_formats/ppm/plain_ppm.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cmath>
 #include <sstream>
 #include "../../data_structures/canvas/canvas.h"
 
@@ -15,10 +17,65 @@ void writeHeader(std::stringstream & ppm, const canvas & c)
 		<< "255\n";
 }
 
+int getColorComponent(float rgb)
+{
+	return std::max(0, std::min(255, static_cast<int>(std::ceil(255 * rgb))));
+}
+
+void writeColorComponent(std::stringstream & ppm, int colorComponent, int & lineLength)
+{
+	int colorComponentLength = colorComponent <= 9 ? 1 : colorComponent <= 99 ? 2 : 3;
+	const int maxLineLength = 70;
+
+	if (lineLength == 0)
+	{
+		ppm << colorComponent;
+		lineLength = colorComponentLength;
+	}
+	else if (lineLength + 1 + colorComponentLength > maxLineLength)
+	{
+		ppm << "\n" << colorComponent;
+		lineLength = colorComponentLength;
+	}
+	else
+	{
+		ppm << " " << colorComponent;
+		lineLength += 1 + colorComponentLength;
+	}
+}
+
+void writePixels(std::stringstream & ppm, const canvas & c)
+{
+	int lineLength = 0;
+	auto width = c.getWidth();
+	auto height = c.getHeight();
+	for (int y = 0; y < height; y++)
+	{
+		if (y != 0)
+		{
+			ppm << "\n";
+			lineLength = 0;
+		}
+
+		for (int x = 0; x < width; x++)
+		{
+			auto pixel = c.getPixelAt(x, y);
+			auto redComponent = getColorComponent(pixel.getRed());
+			writeColorComponent(ppm, redComponent, lineLength);
+			auto greenComponent = getColorComponent(pixel.getGreen());
+			writeColorComponent(ppm, greenComponent, lineLength);
+			auto blueComponent = getColorComponent(pixel.getBlue());
+			writeColorComponent(ppm, blueComponent, lineLength);
+		}
+	}
+}
+
 std::string write(const canvas & c)
 {
 	std::stringstream ppm;
 	writeHeader(ppm, c);
+	writePixels(ppm, c);
+	ppm << "\n";
 	return ppm.str();
 }
 } // namespace plain_ppm
--- cpp/tests/image_formats/plain_ppm.cpp
+++ cpp/tests/image_formats/plain_ppm.cpp
@@ -1,9 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "../framework/catch.hpp"
 #include "../../src/data_structures/canvas/canvas.h"
+#include "../../src/data_structures/color/color.cpp"
 #include "../../src/image_formats/ppm/plain_ppm.cpp"
 
 using data_structures::canvas;
+using data_structures::color;
 
 TEST_CASE("constructing the PPM header")
 {
@@ -17,4 +19,60 @@ TEST_CASE("constructing the PPM header")
 	REQUIRE("5 3" == lineString);
 	std::getline(ppmStream, lineString);
 	REQUIRE("255" == lineString);
+}
+
+TEST_CASE("constructing the PPM pixel data")
+{
+	auto c = canvas(5, 3);
+	auto c1 = color(1.5, 0, 0);
+	auto c2 = color(0, 0.5, 0);
+	auto c3 = color(-0.5, 0, 1);
+	c.setPixelAt(0, 0, c1);
+	c.setPixelAt(2, 1, c2);
+	c.setPixelAt(4, 2, c3);
+	auto ppm = image_formats::plain_ppm::write(c);
+	auto ppmStream = std::stringstream(ppm);
+	std::string lineString;
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0" == lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0" == lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255" == lineString);
+}
+
+TEST_CASE("splitting long lines in PPM files")
+{
+	int width = 10;
+	int height = 2;
+	auto c = canvas(width, height);
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+			c.setPixelAt(x, y, color(1, 0.8, 0.6));
+	}
+	auto ppm = image_formats::plain_ppm::write(c);
+	auto ppmStream = std::stringstream(ppm);
+	std::string lineString;
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" == lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("153 255 204 153 255 204 153 255 204 153 255 204 153" == lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" == lineString);
+	std::getline(ppmStream, lineString);
+	REQUIRE("153 255 204 153 255 204 153 255 204 153 255 204 153" == lineString);
+}
+
+TEST_CASE("ppm files are terminated by a newline character")
+{
+	auto c = canvas(5, 3);
+	auto ppm = image_formats::plain_ppm::write(c);
+	REQUIRE('\n' == ppm[ppm.length() - 1]);
 }
