660aca5f08b08fb54b601b1f654fd794208dd352
Lawrence Rafferty <lawrence.rafferty@gmail.com>
2019-06-06 00:13:08 -0700

    Use different colors for rays cast from increasing heights, to visualize the impact.

--- cpp/src/exercises/cast_rays_at_a_sphere/program.cpp
+++ cpp/src/exercises/cast_rays_at_a_sphere/program.cpp
@@ -17,30 +17,47 @@ using data_structures::matrix;
 using data_structures::ray;
 using shapes::sphere;
 
-int main()
+void castRays(canvas & c, std::shared_ptr<sphere> s, float sphereZ, int multiplier)
 {
-	auto width = 100;
-	auto height = 100;
-	auto c = canvas(width, height);
-
 	auto red = color(1, 0, 0);
+	auto green = color (0, 1, 0);
+	auto blue = color(0, 0, 1);
+	auto yellow = red + green;
+	auto orange = color(1, 0.8, 0);
+	auto purple = red + blue;
+	auto cyan = green + blue;
+	auto rainbow = std::vector<color> { red, orange, yellow, green, cyan, blue, purple };
+	auto pixelColor = rainbow[(multiplier - 1) % rainbow.size()];
 
-	auto s = std::make_shared<sphere>();
-	s->setTransform(matrix::scaling(20, 20, 20).getTranslated(width / 2, height / 2, -40));
-
-	auto rayOrigin = four_tuple::point(width / 2, height / 2, -100);
-	for (int y = 0; y < height; y++)
+	int canvasWidth = c.getWidth();
+	int canvasHeight = c.getHeight();
+	auto rayOrigin = four_tuple::point(canvasWidth / 2, canvasHeight / 2, sphereZ - 30 * multiplier);
+	for (int y = 0; y < canvasHeight; y++)
 	{
-		for (int x = 0; x < width; x++)
+		for (int x = 0; x < canvasWidth; x++)
 		{
 			auto direction = (four_tuple::point(x, y, 0) - rayOrigin).getNormalized();
 			auto r = ray(rayOrigin, direction);
 			auto xs = intersections::find(s, r);
 			auto hit = xs.getHit();
 			if (hit != nullptr)
-				c.setPixelAt(x, y, red);
+				c.setPixelAt(x, y, pixelColor);
 		}
 	}
+}
+
+int main()
+{
+	int width = 100;
+	int height = 100;
+	auto c = canvas(width, height);
+
+	float sphereZ = -20;
+	auto s = std::make_shared<sphere>();
+	s->setTransform(matrix::scaling(20, 20, 20).getTranslated(width / 2, height / 2, sphereZ));
+
+	for (int multiplier = 1; multiplier <= 7; multiplier++)
+		castRays(c, s, sphereZ, multiplier);
 
 	auto ppm = image_formats::plain_ppm::write(c);
 	image_formats::file_utility::overwrite("./cpp/src/exercises/cast_rays_at_a_sphere/cast_rays_at_a_sphere.ppm", ppm);
